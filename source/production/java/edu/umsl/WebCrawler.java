d5a7f5d74ff9dc580bdc5a4065cf86329e6b2bb8
lcwdmg <lcwdmg@umsystem.edu>
2021-04-20 21:19:47 -0400

    goes through wikipedia links, prints out title and word counts for each

--- source/production/java/edu/umsl/WebCrawler.java
+++ source/production/java/edu/umsl/WebCrawler.java
@@ -6,9 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class WebCrawler {
     public static void main(String[] args) {
@@ -20,9 +18,12 @@ public class WebCrawler {
 
     public static void crawler (String startingURL) {
         final int MAX = 100;
+
         ArrayList<String> listOfPendingURLs = new ArrayList<>();
         HashSet<String> listOfTraversedURLs = new HashSet<>();
         HashSet<String> titles = new HashSet<>();
+        HashMap<String, Integer>  wordCounts = new HashMap<>();
+
 
         listOfPendingURLs.add(startingURL);
 
@@ -30,7 +31,7 @@ public class WebCrawler {
         while (!listOfPendingURLs.isEmpty() && listOfTraversedURLs.size() <= MAX) {
             //sleep a bit
             try {
-                Thread.sleep(50);
+                Thread.sleep(5);
             } catch (InterruptedException ex){
                 //do nothing
             }
@@ -40,11 +41,14 @@ public class WebCrawler {
             if (!listOfTraversedURLs.contains(urlString)) {
                 listOfTraversedURLs.add(urlString);
 
-                System.out.println("Crawl " + urlString);
                 try {
                     Document doc = Jsoup.connect(urlString).get(); //try to connect
+
                     String title = doc.title();
                     System.out.println(title);
+
+                    System.out.println("\t" + urlString);
+                    System.out.println("\t" + getWordCount(doc));
 
                     //add the next urls to the list
                     for (String s: getSubURls(doc)) {
@@ -55,10 +59,37 @@ public class WebCrawler {
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
-
-
             }
         }
+    }
+
+    public static ArrayList<String> getWords (Document document) {
+        ArrayList<String> wordList = new ArrayList<>();
+
+        Elements paragraphs = document.select("p");
+
+        for (Element paragraph : paragraphs) {
+            String words  = paragraph.ownText();
+            //System.out.println(words);
+            wordList.addAll(Arrays.asList(words.split(" ")));
+        }
+        return wordList;
+    }
+    public static HashMap<String, Integer> getWordCount (Document doc) {
+        ArrayList<String> wordList = getWords(doc);
+        HashMap<String, Integer>  wordCounts = new HashMap<>();
+
+        //Add the array of words to the map
+        for (String s : wordList) {
+            if (wordCounts.containsKey(s)) {
+                int newValue = Integer.parseInt(String.valueOf(wordCounts.get(s)));
+                newValue++;
+                wordCounts.put(s, newValue);
+            } else {
+                wordCounts.put(s, 1);
+            }
+        }
+        return wordCounts;
     }
 
     public static ArrayList<String> getSubURls (Document document) {
@@ -71,7 +102,6 @@ public class WebCrawler {
             if (link.attr("href").startsWith("/wiki/")) { //use only internal wikipedia links
                 String fullLinkName = "https://en.wikipedia.org" + link.attr("href");
                 list.add(fullLinkName);
-                //System.out.println("link : " + fullLinkName);
             }
         }
         return list;
